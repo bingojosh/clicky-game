21506497b08e9deb70fae7a8be8e1a09074b4cf1
Josh LaRochelle <joshua.r.larochelle@gmail.com>
2019-04-08 17:28:26 -0400

    Updates

---
+++ precache-manifest.5d29688a2db47cec2abc99d830acc236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "13ca7e3e220917ff9ed0",
+    "url": "/clicky-game/static/js/main.77507169.chunk.js"
+  },
+  {
+    "revision": "2a74e85a7b3e98fa7364",
+    "url": "/clicky-game/static/js/2.53287691.chunk.js"
+  },
+  {
+    "revision": "13ca7e3e220917ff9ed0",
+    "url": "/clicky-game/static/css/main.f61eb360.chunk.css"
+  },
+  {
+    "revision": "2a74e85a7b3e98fa7364",
+    "url": "/clicky-game/static/css/2.b72c0bbe.chunk.css"
+  },
+  {
+    "revision": "61bfef0442279d186c4abec5c8963975",
+    "url": "/clicky-game/index.html"
+  }
+];
